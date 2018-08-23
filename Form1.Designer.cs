2c9adb20431e093e02cb6eadb5ac347b03f7f2e9
Angela Pugh <39738603+AngelaPugh@users.noreply.github.com>
2018-08-23 18:14:49 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace GTCA_TextDecrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DecryptionButton = new System.Windows.Forms.Button();
+            this.DecryptLabel = new System.Windows.Forms.Label();
+            this.DecryptKeyTextBox = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "encrypted files (*.enc)|*.enc";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Lime;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(954, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(631, 649);
+            this.textBox1.TabIndex = 1;
+            // 
+            // DecryptionButton
+            // 
+            this.DecryptionButton.BackColor = System.Drawing.Color.Black;
+            this.DecryptionButton.Enabled = false;
+            this.DecryptionButton.Location = new System.Drawing.Point(695, 510);
+            this.DecryptionButton.Name = "DecryptionButton";
+            this.DecryptionButton.Size = new System.Drawing.Size(159, 107);
+            this.DecryptionButton.TabIndex = 2;
+            this.DecryptionButton.Text = "Decode File Message";
+            this.DecryptionButton.UseVisualStyleBackColor = false;
+            this.DecryptionButton.Click += new System.EventHandler(this.DecryptionButton_Click);
+            // 
+            // DecryptLabel
+            // 
+            this.DecryptLabel.AutoSize = true;
+            this.DecryptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecryptLabel.Location = new System.Drawing.Point(689, 160);
+            this.DecryptLabel.Name = "DecryptLabel";
+            this.DecryptLabel.Size = new System.Drawing.Size(221, 31);
+            this.DecryptLabel.TabIndex = 3;
+            this.DecryptLabel.Text = "Decryption Key : ";
+            // 
+            // DecryptKeyTextBox
+            // 
+            this.DecryptKeyTextBox.Location = new System.Drawing.Point(728, 204);
+            this.DecryptKeyTextBox.Multiline = true;
+            this.DecryptKeyTextBox.Name = "DecryptKeyTextBox";
+            this.DecryptKeyTextBox.Size = new System.Drawing.Size(138, 65);
+            this.DecryptKeyTextBox.TabIndex = 4;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text files|*.txt|Word Files|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(954, 754);
+            this.Controls.Add(this.DecryptKeyTextBox);
+            this.Controls.Add(this.DecryptLabel);
+            this.Controls.Add(this.DecryptionButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "GTCA-Decrypt";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button DecryptionButton;
+        private System.Windows.Forms.Label DecryptLabel;
+        private System.Windows.Forms.TextBox DecryptKeyTextBox;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GTCA_TextDecrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+            string filetext = "";     
+
+                //Opens the file dialog to allow user to choose a file to open in the program.
+                StreamReader infile = new StreamReader(openFileDialog1.FileName);
+
+                //Reads through the file text until the EndOfStream is reached.
+                while (!infile.EndOfStream)
+                {
+                    //Reads each character.
+                    filetext += (char)infile.Read();
+                }
+
+                //Closes the textFile.
+                infile.Close();
+
+                //Displays the file from the text to textBox1.
+                textBox1.Text = filetext;
+            DecryptionButton.Enabled = true;
+        }
+
+        private void DecryptionButton_Click(object sender, EventArgs e)
+        {
+       
+            string DecryptedString = "";
+            int Key;
+
+            if(! int.TryParse(DecryptKeyTextBox.Text, out Key))
+            {
+                MessageBox.Show("Unable to Decrypt message with key entered.");
+            }
+            
+
+           foreach(char Letter in textBox1.Text)
+            {
+                char DecryptedLetter = Convert.ToChar(Letter - Key);
+                DecryptedString += DecryptedLetter.ToString();
+            }
+
+            textBox1.Text = DecryptedString;
+            DecryptionButton.Enabled = false;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            StreamWriter writeData = new StreamWriter(saveFileDialog1.FileName);
+
+            foreach (char Letter in textBox1.Text)
+            {
+                writeData.Write(Letter);
+            }
+
+            writeData.Close();
+
+        }
+
+       
+    }
+}
